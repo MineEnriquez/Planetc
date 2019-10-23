7338bfee0301ca8d5a682a4e37b7fc23db5f1337
MineEnriquez <38847090+MineEnriquez@users.noreply.github.com>
2019-10-23 00:50:20 -0700

    Adding products

--- Models/Product.cs
+++ Models/Product.cs
@@ -11,6 +11,8 @@ namespace Planetc.Models
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public int Quantity { get; set; }
+        public string ImageFileName { get; set; }
+        public string Description { get; set; }
     }
 }
 
